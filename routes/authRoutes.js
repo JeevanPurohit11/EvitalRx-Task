80a750f186f53d2723ae7b39ea5052479de674af
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2025-01-07 23:55:24 +0530

    Task

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,15 +4,13 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Auth Routes
+
 router.post('/signup', authController.signup);
 router.post('/verify-otp', authController.verifyOTP);
 router.post('/login', authController.login);
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
 
-// User Routes
-router.get('/profile', authMiddleware, userController.getProfile);
-router.put('/update-profile', authMiddleware, userController.updateProfile);
+
+
+
 
 module.exports = router;
